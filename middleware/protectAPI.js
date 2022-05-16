6fdd3cc401605153e19b6c0514f9bca1ececc48f
moshe-eichler <eichlermoshe@gmail.com>
2022-05-16 19:16:04 +0300

    idlware

--- middleware/protectAPI.js
+++ middleware/protectAPI.js
@@ -1,8 +1,8 @@
 const protectAPI = (handler) => {
     return async (req, res) => {
-        if(req.headers['user-agent'] != 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)' & req.headers['sec-fetch-mode'] != 'cors') {
-            return res.status(403).json({success: false, message: `Forbidden`});
-        }
+        // if(req.headers['user-agent'] != 'node-fetch/1.0 (+https://github.com/bitinn/node-fetch)' & req.headers['sec-fetch-mode'] != 'cors') {
+        //     return res.status(403).json({success: false, message: `Forbidden`});
+        // }
         return handler(req, res);
     }
 }
