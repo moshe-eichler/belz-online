564f468c2b379fa9334457de7d7ca3cdcbd596af
moshe-eichler <eichlermoshe@gmail.com>
2022-05-28 23:14:42 +0300

    slick

--- components/SideAdvertising.js
+++ components/SideAdvertising.js
@@ -1,4 +1,19 @@
 import Image from 'next/image';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 7000,
+    autoplaySpeed: 500,
+    vertical: true,
+    cssEase: "ease"
+};
 
 export default function SideAdvertising({ side }) {
     function importAll(r) {
@@ -6,15 +21,15 @@ export default function SideAdvertising({ side }) {
     }
     const images = (side == 'right') ? importAll(require.context('../public/advertising/right', false, /\.(png|jpe?g|svg)$/)) : importAll(require.context('../public/advertising/left', false, /\.(png|jpe?g|svg)$/));
 
-
-    
-    return(
+    return (
         <>
-            {images.map((image, i) => 
-                <div key={i} style={{ padding: "10%", border: "1px solid black"}}>
-                    <Image key={i} src={image.default.src} layout="responsive" width={100} height={120} />
-                </div>
-            )}
+            <Slider {...settings}>
+                {images.map((image, i) => 
+                    <div key={i} className={`side-ad`}>
+                        <Image key={i} src={image.default.src} layout="responsive" width={100} height={120} />
+                    </div>
+                )}
+            </Slider>
         </>
-    )
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,7 @@ export default function Home(props) {
     }
 
     return (
+
         <>
             <Head>
                 <title>רשימת אנ״ש</title>
@@ -100,7 +101,6 @@ export default function Home(props) {
 }
 
 export const getStaticProps = async () => {
-    // console.log(process.env);
     const url = `https://anash.vercel.app/api/members?limit=40`
     // const url = `http://localhost:3000/api/members?limit=40`
     const data = await fetch(url)
