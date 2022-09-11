848aee730dac89609a1619ceed6b9714a08ec268
moshe-eichler <eichlermoshe@gmail.com>
2022-09-11 13:21:07 +0300

    more fixes

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,5 +1,5 @@
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import { Button } from 'react-bootstrap';
 import { useState } from 'react';
