902dd6062a21a6d0a29b2aea157c296c5d18116c
Moshe Eichler <78307768+MosheEichler@users.noreply.github.com>
2023-02-01 11:13:54 +0200

    Update Members.js
    remove limit

--- components/Members.js
+++ components/Members.js
@@ -12,7 +12,7 @@ export default function Members({ data, filters, modalFunction}) {
     const [noFound, setNoFound] = useState('');
 
     const getMembers = async () => { 
-        const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/api/content?source=anash_belz&limit=40`);
+        const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/api/content?source=anash_belz`);
         url.searchParams.append('limit', 40);
         url.searchParams.append('skip', members.length);
         if (filters) url.searchParams.append('querySearch', filters);
