b97fe103538c0b400de172e64eb91a2dbc930fec
Moshe Eichler <78307768+MosheEichler@users.noreply.github.com>
2023-11-04 19:50:23 +0200

    Update limit content.js

--- pages/api/content.js
+++ pages/api/content.js
@@ -6,7 +6,7 @@ const getContent = async (req, res) => {
     let queryObject = url.parse(req.url, true).query;
     let querySearch = '';
     if (queryObject.querySearch) querySearch = '"' + queryObject.querySearch.replace(/ /g, '" "') + '"';
-    let limit = queryObject.limit ? Math.min(queryObject.limit, 40) : 40; // To avoid scraping huge records.
+    let limit = queryObject.limit ? Math.max(queryObject.limit, 40) : 40; // To avoid scraping huge records.
     let skip = queryObject.skip ? Math.max(queryObject.skip, 0) : 0; // To avoid skip in minus
     let source = queryObject.source
 
